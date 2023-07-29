05f6e48f564dd21e59df8b89aa3fbf83a57d8f40
Leonardo Asad <leonardo.asad.89@gmail.com>
2023-07-29 23:24:53 +1200

    server side rendering

--- components/dropdown.user.tsx
+++ components/dropdown.user.tsx
@@ -1,59 +1,79 @@
+"use client";
+import { useState } from "react";
 import clsx from "clsx";
 
-interface Props {
-  openUserMenu: boolean;
-}
+export default function DropdownUser() {
+  const [openUserMenu, setOpenUserMenu] = useState(false);
 
-export default function DropdownUser({ openUserMenu }: Props) {
   return (
-    <div
-      id="dropdown-user"
-      className={clsx(
-        "z-50 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600",
-        "absolute top-5 right-0",
-        openUserMenu ? "block" : "hidden"
-      )}
-    >
-      <div className="px-4 py-3" role="none">
-        <p className="text-sm text-gray-900 dark:text-white" role="none">
-          Neil Sims
-        </p>
-        <p
-          className="text-sm font-medium text-gray-900 truncate dark:text-gray-300"
-          role="none"
+    <div className="flex items-center relative">
+      <div className="flex items-center ml-3">
+        <div>
+          <button
+            onClick={() => setOpenUserMenu(!openUserMenu)}
+            type="button"
+            className="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+            aria-expanded="false"
+            data-dropdown-toggle="dropdown-user"
+          >
+            <span className="sr-only">Open user menu</span>
+            <img
+              className="w-8 h-8 rounded-full"
+              src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+              alt="user photo"
+            />
+          </button>
+        </div>
+        <div
+          id="dropdown-user"
+          className={clsx(
+            "z-50 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600",
+            "absolute top-4 right-0",
+            openUserMenu ? "block" : "hidden"
+          )}
         >
-          neil.sims@flowbite.com
-        </p>
+          <div className="px-4 py-3" role="none">
+            <p className="text-sm text-gray-900 dark:text-white" role="none">
+              Neil Sims
+            </p>
+            <p
+              className="text-sm font-medium text-gray-900 truncate dark:text-gray-300"
+              role="none"
+            >
+              neil.sims@flowbite.com
+            </p>
+          </div>
+          <ul className="py-1" role="none">
+            <li>
+              <a
+                href="#"
+                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+                role="menuitem"
+              >
+                Settings
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+                role="menuitem"
+              >
+                Earnings
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+                role="menuitem"
+              >
+                Sign out
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <ul className="py-1" role="none">
-        <li>
-          <a
-            href="#"
-            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-            role="menuitem"
-          >
-            Settings
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-            role="menuitem"
-          >
-            Earnings
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-            role="menuitem"
-          >
-            Sign out
-          </a>
-        </li>
-      </ul>
     </div>
   );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,5 @@
+import DropdownUser from "./dropdown.user";
+
 export default function Navbar() {
   return (
     <nav className="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
@@ -15,27 +17,7 @@ export default function Navbar() {
               </span>
             </a>
           </div>
-          {/* <div className="flex items-center relative">
-            <div className="flex items-center ml-3">
-              <div>
-                <button
-                  onClick={() => setOpenUserMenu(!openUserMenu)}
-                  type="button"
-                  className="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-                  aria-expanded="false"
-                  data-dropdown-toggle="dropdown-user"
-                >
-                  <span className="sr-only">Open user menu</span>
-                  <img
-                    className="w-8 h-8 rounded-full"
-                    src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-                    alt="user photo"
-                  />
-                </button>
-              </div>
-              <DropdownUser openUserMenu={openUserMenu} />
-            </div>
-          </div> */}
+          <DropdownUser />
         </div>
       </div>
     </nav>
