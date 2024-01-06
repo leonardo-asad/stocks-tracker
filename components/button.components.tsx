31aa8819ce7af5f6f50b91538ef3a8320e2e574f
Leonardo Asad <leonardo.asad.89@gmail.com>
2024-01-06 22:31:36 +1300

    refactor login button

--- components/button.components.tsx
+++ components/button.components.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import { signIn, signOut } from "next-auth/react";
 
-export const LoginButton = () => {
+export const LoginButton = ({ children }: { children: React.ReactNode }) => {
   return (
     <button
       onClick={() =>
@@ -13,7 +13,7 @@ export const LoginButton = () => {
       }
       className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
     >
-      Sign in
+      {children}
     </button>
   );
 };
