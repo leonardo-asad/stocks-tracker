ae17053df0bb33ad8c38e63d14e78ce7fc03422f
Leonardo Asad <leonardo.asad.89@gmail.com>
2024-01-06 22:30:28 +1300

    refactor landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/header";
+import { Button } from "@/components/button";
 
 export default async function Home() {
   return (
@@ -14,19 +15,29 @@ export default async function Home() {
         </header>
         <main className="p-10">
           <section>
-            <h2 className="text-4xl font-bold mb-4">About the Application</h2>
-            <p className="text-xl mb-2">
-              Stock Tracker allows you to monitor the stock market in real-time.
-              Stay updated with the latest prices of your favorite stocks.
-            </p>
-            <p className="text-xl mb-2">
-              Manage your portfolio with ease. Get a visual and quantitative
-              glimpse of your portfolio composition.
-            </p>
-            <p className="text-xl">
-              Make informed decisions with our analysis tools and stay ahead in
-              the game.
-            </p>
+            <div>
+              <h2 className="text-4xl font-bold mb-4">About the Application</h2>
+              <p className="text-xl mb-2">
+                Stock Tracker allows you to monitor the stock market in
+                real-time. Stay updated with the latest prices of your favorite
+                stocks.
+              </p>
+              <p className="text-xl mb-2">
+                Manage your portfolio with ease. Get a visual and quantitative
+                glimpse of your portfolio composition.
+              </p>
+              <p className="text-xl">
+                Make informed decisions with our analysis tools and stay ahead
+                in the game.
+              </p>
+            </div>
+            <div className="flex items-center justify-center">
+              <a href="/register">
+                <Button className="m-8" size="xxl" variant="default">
+                  Subscribe
+                </Button>
+              </a>
+            </div>
           </section>
         </main>
       </div>
